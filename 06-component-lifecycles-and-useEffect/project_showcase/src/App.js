077f91e9d8037213f9b99aa344c5ef0258301d82
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-01-06 16:12:53 -0500

    post-class lecture code ready to share out

--- 06-component-lifecycles-and-useEffect/project_showcase/src/App.js
+++ 06-component-lifecycles-and-useEffect/project_showcase/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
@@ -7,19 +7,44 @@ const App = () => {
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
 
-  const handleClick = () => {
+
+  const getProjects = () => {
     fetch("http://localhost:4000/projects")
-      .then((res) => res.json())
-      .then((projects) => setProjects(projects));
-  };
+    .then((res) => res.json())
+    .then((projects) => setProjects(projects));
+  }
+
+  useEffect(() => {
+      // console.log("use effect firing")
+      getProjects()
+  }, [])
+
+
+  useEffect(() => {
+    console.log("Magic 8 Ball, am i in dark mode? The answer is: ", isDarkMode)
+  }, [isDarkMode])
+
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {console.log("setting interval")}, 3000)
+    console.log("id for this setInterval is: ", intervalId)
+
+    return () => {
+      console.log("cleaning up now")
+      clearInterval(intervalId)
+    }
+
+  }, [isDarkMode])
 
   const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
+
+
+  if(!projects.length) return (<h1>...Loading....</h1>)
 
   return (
     <div className={isDarkMode ? "App" : "App light"}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
       <ProjectForm />
-      <button onClick={handleClick}>Load Projects</button>
       <ProjectList projects={projects} />
     </div>
   );
