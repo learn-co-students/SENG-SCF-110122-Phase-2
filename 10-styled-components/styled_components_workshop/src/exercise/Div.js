489122f1268e510cc7f97b9750c8add0475d6ea1
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-01-13 12:28:23 -0500

    code ready for lecture

---
+++ 10-styled-components/styled_components_workshop/src/exercise/Div.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Div = styled.div`
+  width: 300px;
+  height: 300px;
+  display: grid;
+  place-content: center;
+  gap: 16px;
+  background: skyblue;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.15);
+`;
