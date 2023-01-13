6cb097a3035ba135289df5e297ac628d7b83d0ac
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-01-13 13:22:45 -0500

    more fixes to styled code

--- 10-styled-components/styled_components_workshop/src/exercise/01.js
+++ 10-styled-components/styled_components_workshop/src/exercise/01.js
@@ -1,48 +1,54 @@
 import { useState } from "react";
 // ✅ remember to import the styled-components library!
-import styled from "styled-components";
-
 // ✅ create two components using styled-components: a div and a button
 // ✅ use the inline styles from the elements below to create the CSS rules for your components
 // 📃 https://styled-components.com/docs/basics#getting-started
 
 export default function App() {
   const [count, setCount] = useState(0);
-  
+
   // ✅ remove the style prop from the div and the button
   // ✅ replace the JSX elements with your styled components
   return (
-    <Div>
-      <Button onClick={() => setCount((count) => count + 1)} >
+    <div
+      style={{
+        width: "300px",
+        height: "300px",
+        display: "grid",
+        placeContent: "center",
+        gap: "16px",
+        background: "lightgreen",
+        boxShadow: "2px 2px 8px rgba(0, 0, 0, 0.15)",
+      }}
+    >
+      <button
+        onClick={() => setCount(count => count + 1)}
+        style={{
+          background: "orange",
+          padding: "1rem",
+          fontSize: "1em",
+          border: "2px solid black",
+          border: "2px solid green",
+          borderRadius: "4px",
+          cursor: "pointer",
+        }}
+      >
         Clicks: {count}
-      </Button>
-      <Button onClick={() => setCount((count) => count + 1)} >
+      </button>
+      <button
+        onClick={() => setCount(count => count + 1)}
+        style={{
+          background: "orange",
+          padding: "1rem",
+          fontSize: "1em",
+          border: "2px solid black",
+          border: "2px solid green",
+          borderRadius: "4px",
+          cursor: "pointer",
+        }}
+      >
         Clicks: {count}
-      </Button>
-    </Div>
+      </button>
+    </div>
   );
 }
-
-const Div = styled.div`
-  width: 300px;
-  height: 300px;
-  display: grid;
-  place-content: center;
-  gap: 16px;
-  background: lightgreen;
-  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.15);
-`
-
-const Button = styled.button`
-  background: orange;
-  padding: 1rem;
-  font-size: 1em;
-  border: 2px solid black;
-  border: 2px solid green;
-  border-radius: 4px;
-  cursor: pointer;
-    &:hover {
-      background: blue;
-      color: white;
-    }
-`
